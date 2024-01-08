99e4dc3b3c925a98c6429de33afdf4d3825e8a89
Himanshu <thisivylife01@gmail.com>
2024-01-08 15:22:33 +0530

    Project Control Error Resolved

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Footer, Header } from './components'
 import { Outlet, useLocation } from 'react-router-dom'
 
@@ -6,41 +6,37 @@ const Layout = () => {
   const location = useLocation();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-
-
   useEffect(() => {
     const cursorDiv = document.getElementById('cursorDiv');
-    // console.log(location);
-    // console.log('Use Effect Called from Layout');
-  
-    const updatePosition = (e) => {
-      cursorDiv.style.top = e?.pageY + 'px';
-      cursorDiv.style.left = e?.pageX + 'px';
-    };
+    if (cursorDiv) {
+      const updatePosition = (e) => {
+        cursorDiv.style.top = e?.pageY + 'px';
+        cursorDiv.style.left = e?.pageX + 'px';
+      };
 
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-  
-    document.addEventListener('mousemove', updatePosition);
-    window.addEventListener('resize', handleResize);
-  
-    // Scroll to top of page on location change
-    window.scrollTo(0, 0);
-  
-    return () => {
-      document.removeEventListener('mousemove', updatePosition);
-      window.removeEventListener('resize', handleResize);
-    };
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+
+      document.addEventListener('mousemove', updatePosition);
+      window.addEventListener('resize', handleResize);
+
+      // Scroll to top of page on location change
+      window.scrollTo(0, 0);
+
+      return () => {
+        document.removeEventListener('mousemove', updatePosition);
+        window.removeEventListener('resize', handleResize);
+      };
+    }
   }, [location]);
-
 
   const cursorDivStyle = {
     position: 'absolute',
     pointerEvents: 'none',
-    width: '60px',
-    height: '60px',
-    filter: 'blur(65px)',
+    width: '100px',
+    height: '100px',
+    filter: 'blur(50px)',
     borderRadius: '50%',
     backgroundColor: 'var(--primary)',
     zIndex: '9999',
